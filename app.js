d212f7f43c9891427f089a6f70dda162267b22db
saurav-max <sauravmax237@gmail.com>
2020-12-14 13:20:25 +0530

    feat: take input value after listening enter key

---
+++ app.js
@@ -0,0 +1,10 @@
+const searchEl = document.querySelector(".search");
+
+function searchWeather(e){
+    if(e.keyCode == 13){
+        
+    }
+    
+}
+
+searchEl.addEventListener('keyup', searchWeather);
